aad7cd6240703398616f29c6d13121587867b595
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-12-10 10:21:20 -0500

    lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,11 +5,15 @@
 def canUnlockAll(boxes):
     """Lockboxes"""
     open = [0]
-    for x in boxes:
-        for sublist in boxes:
-            for item in sublist:
-                if boxes.index(sublist)in open:
-                    open.append(item)
+    
+    for sublist in boxes:
+        for item in sublist:
+            if boxes.index(sublist)in open:
+                open.append(item)
+    for sublist in boxes:
+        for item in sublist:
+            if boxes.index(sublist)in open:
+                open.append(item)            
     q = set(open)
     for z in range(1, len(boxes)-1):
         if z not in q:
