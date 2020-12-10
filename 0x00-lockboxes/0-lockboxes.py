4dea0bc373c9fd9a4bc0b0952d9af0a73ca4cfe5
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-12-10 10:24:02 -0500

    lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,7 +5,6 @@
 def canUnlockAll(boxes):
     """Lockboxes"""
     open = [0]
-    
     for sublist in boxes:
         for item in sublist:
             if boxes.index(sublist)in open:
@@ -13,7 +12,7 @@ def canUnlockAll(boxes):
     for sublist in boxes:
         for item in sublist:
             if boxes.index(sublist)in open:
-                open.append(item)            
+                open.append(item)
     q = set(open)
     for z in range(1, len(boxes)-1):
         if z not in q:
