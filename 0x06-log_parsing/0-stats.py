444eb2796d01acdbc3d472e8ffb73fd09533ca8f
Bzhamdi <bouzouitina.hamdi@gmail.com>
2021-01-28 21:58:18 -0500

    0x06-log_parsing/

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+script that reads stdin line by line and computes metrics
+"""
+
+
+import sys
+
+file_size = 0
+mydict = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0}
+
+try:
+    for x, line in enumerate(sys.stdin, 1):
+        spline = line.split()
+        spline_lenth = len(spline)
+        if spline_lenth > 2:
+            statuscode = spline[spline_lenth - 2]
+            file_size += int(spline[spline_lenth - 1])
+            if statuscode in mydict:
+                mydict[str(statuscode)] += 1
+        if x % 10 == 0:
+            print("File size: {}".format(file_size))
+            for i in sorted(mydict):
+                if mydict[i] > 0:
+                    print("{}: {}".format(i, mydict[i]))
+
+except KeyboardInterrupt:
+    print("File size: {:d}".format(file_size))
+    for key in sorted(mydict.keys()):
+        if mydict[key] != 0:
+            print("{}: {:d}".format(key, mydict[key]))
+    raise
