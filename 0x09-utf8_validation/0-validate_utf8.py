4a20c52a9d1002ba9d98b07821bea35dfa17c6e5
Bzhamdi <bouzouitina.hamdi@gmail.com>
2021-02-19 17:53:05 -0500

    utf-8

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -16,18 +16,8 @@ def validUTF8(data):
     0000 0800-0000 FFFF | 1110xxxx 10xxxxxx 10xxxxxx
     0001 0000-0010 FFFF | 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
     """
-    c = 0
-    for x in data:
-        if c == 0:
-            if (x >> 5) == 0b110:
-                c = 1
-            elif (x >> 4) == 0b1110:
-                c = 2
-            elif (x >> 3) == 0b11110:
-                c = 3
-
-        else:
-            if (x >> 6) != 0b10:
-                return False
-            c -= 1
-    return c == 0
+    try:
+        bytes([x & 255 for x in data]).decode("UTF-8")
+        return True
+    except Exception:
+        return False
